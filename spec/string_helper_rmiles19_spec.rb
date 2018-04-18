b44058a4d14430051d0df5160f6bd686b18a0245
Rachel Miles <milesrachel9@gmail.com>
2018-04-18 10:57:08 -0600

    deploy gem

--- spec/string_helper_rmiles19_spec.rb
+++ spec/string_helper_rmiles19_spec.rb
@@ -1,9 +1,20 @@
 RSpec.describe StringHelperRmiles19 do
+  subject { StringHelperRmiles19 }
+  let(:str) { 'My String' }
+
   it "has a version number" do
     expect(StringHelperRmiles19::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
+  it 'reverses a string' do
+    actual = subject.reversify(str)
+    expected = 'gnirtS yM'
+    expect(actual).to eq(expected)
+  end 
+
+  it 'adds spaces' do
+    actual = subject.spacify(str, 1)
+    expected = 'M y   S t r i n g'
+    expect(actual).to eq(expected)
+  end 
 end
